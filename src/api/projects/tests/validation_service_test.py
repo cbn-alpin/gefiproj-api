b144ddd39ad9669fa9e82f4b765bc49ce3e2a444
cashenvic <comptes.kh@protonmail.com>
2020-12-06 03:16:24 +0100

    [project] test validation

--- src/api/projects/tests/validation_service_test.py
+++ src/api/projects/tests/validation_service_test.py
@@ -1,10 +1,25 @@
-from ..validation_service import ProjectValidationService
+import unittest
+
+from src.api.projects.validation_service import ProjectValidationService
 
 
-class MyTestCase(unittest.TestCase):
+class ProjectValidationServiceTestCase(unittest.TestCase):
     def test_validate_get_all(self):
-        validation = ProjectValidationService.validate_get_all({'some'})
-        self.assertEqual(False, False)
+        validation_errors = ProjectValidationService.validate_get_all({'limit': 'some limit', 'offset': 'some offset'})
+        self.assertEqual(len(validation_errors), 2)
+
+        validation_errors = ProjectValidationService.validate_get_all({'limit': '10', 'offset': '20'})
+        self.assertEqual(len(validation_errors), 0)
+
+    def test_validate_post(self):
+        project = {'code_p': 'test', 'nom_p': 'Project TEST', 'statut_p': 'true', 'id_u': 4}
+        validation_errors = ProjectValidationService.validate_post(project)
+        self.assertEqual(len(validation_errors), 0)
+
+        project['statut_p'] = 'terminé'
+        validation_errors = ProjectValidationService.validate_post(project)
+        self.assertEqual(len(validation_errors), 1)
+        self.assertEqual(validation_errors[0]['field'], 'statut_p')
 
 
 if __name__ == '__main__':
